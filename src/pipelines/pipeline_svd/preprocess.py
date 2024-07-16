a1436126010b59790bd98e985d33b8fe439b1351
PH <jnes40722@gmail.com>
2024-07-16 11:58:33 +0900

    Fix loading data part in preprocess (SVD)

--- src/pipelines/pipeline_svd/preprocess.py
+++ src/pipelines/pipeline_svd/preprocess.py
@@ -1,31 +1,19 @@
 import asyncio
-
-import pandas as pd
-from datareader.ml_1m_data import Ratings
-from surprise import Dataset, Reader
-from surprise.model_selection import train_test_split
+from datareader.ml_10m_data import Ratings, Rating
 
 
 async def get_data_from_db():
     # fetch the data from db
     ratings = await Ratings.from_db()
-    return ratings.data
+    return ratings
 
 
-def preprocess(test_size):
+def preprocess(test_size) -> tuple[list[Rating], list[Rating]]:
     # Load the training data from db
     ratings = asyncio.run(get_data_from_db())
 
-    # Convert the training data to pd.Dataframe
-    ratings_df = pd.DataFrame([rating.model_dump() for rating in ratings])
-
-    # Define a Reader object
-    reader = Reader(rating_scale=(1, 5))
-
-    # Load the data into a Surprise dataset
-    data = Dataset.load_from_df(ratings_df[["user_id", "item_id", "rating"]], reader)
-
     # Split the dataset into train and test sets
-    trainset, testset = train_test_split(data, test_size=test_size)
+    # train_data, test_data = ratings.split_data()
+    traindata, testdata = ratings.split_data(test_size=test_size)
 
-    return trainset, testset
+    return traindata, testdata
