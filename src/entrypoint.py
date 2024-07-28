360dbea165b01eab0acb6943141da7cab72d3eca
PH <jnes40722@gmail.com>
2024-07-28 16:07:22 +0900

    Fix docker-compose: approach to build pipeline container

---
+++ src/entrypoint.py
@@ -0,0 +1,20 @@
+import os
+import sys
+import pipelines
+import pipelines.pipeline_random
+import pipelines.pipeline_svd
+
+
+def run_pipeline(pipeline_name):
+    if pipeline_name == "RandomRecommender":
+        pipelines.pipeline_random.run_pipeline()
+    elif pipeline_name == "SVD":
+        pipelines.pipeline_svd.run_pipeline()
+    else:
+        print(f"Unknown pipeline: {pipeline_name}")
+        sys.exit(1)
+
+
+if __name__ == "__main__":
+    pipeline_name = os.getenv("PIPELINE_NAME", "RandomRecommender")
+    run_pipeline(pipeline_name)
--- src/pipelines/pipeline_svd.py
+++ src/pipelines/pipeline_svd.py
@@ -3,6 +3,14 @@ import asyncio
 import pandas as pd
 from datareader.ml_10m_data import IntegratedData, IntegratedDatas
 from surprise import SVD, Dataset, Reader, accuracy
+import joblib
+from core.config import DIR_PATH, settings
+from utils.pipeline_logging import configure_logging
+
+import mlflow
+
+# Settings of logging output
+logger = configure_logging()
 
 
 async def get_data_from_db(user_num=1000):
@@ -61,6 +69,41 @@ def evaluation_model(algo, testset):
     predictions = algo.test(testset)
     rmse = accuracy.rmse(predictions)
     return rmse
+
+
+def run_pipeline(user_num=1000):
+    # Logging the experiment details
+    logger.info(f"MLflow tracking uri: {settings.tracking_uri}")
+    logger.info(f"artifact root: {settings.artifact_location}")
+    logger.info(f"experiment id: {settings.experiment_id}")
+    logger.info(f"experiment name: {settings.experiment_name}")
+
+    # Load the tracking uri and the experiment name
+    tracking_uri = settings.tracking_uri
+
+    # Set the tracking URI to the local MLflow server
+    mlflow.set_tracking_uri(tracking_uri)
+
+    with mlflow.start_run(experiment_id=settings.experiment_id) as run:
+        # Preprocess
+        trainset, testset = preprocess(user_num)
+        mlflow.log_param("user_num", user_num)
+        mlflow.log_param("model_name", "SVD")
+        mlflow.log_param("dataset", "ml-10m")
+
+        # model training
+        algo = train_model(trainset)
+
+        # Save the model locally
+        model_filename = DIR_PATH / "mlflow/artifacts/svd_model.pkl"
+        joblib.dump(algo, model_filename)
+        print(f"Model saved to {model_filename}")
+        mlflow.log_artifact(model_filename, artifact_path="models")
+
+        # predict
+        rmse = evaluation_model(algo, testset)
+        mlflow.log_metric("rmse", rmse)
+        logger.info(f"rmse: {rmse}")
 
 
 if __name__ == "__main__":
