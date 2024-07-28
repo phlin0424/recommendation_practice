a9233b2f57f3bccd39ae1b8a9f3004378810c1c9
PH <phlin@example.com>
2024-07-28 17:25:45 +0900

    Add pipeline_popularity

--- src/entrypoint.py
+++ src/entrypoint.py
@@ -1,6 +1,6 @@
 import os
 import sys
-import pipelines
+import pipelines.pipeline_popularity
 import pipelines.pipeline_random
 import pipelines.pipeline_svd
 
@@ -8,6 +8,8 @@ import pipelines.pipeline_svd
 def run_pipeline(pipeline_name):
     if pipeline_name == "RandomRecommender":
         pipelines.pipeline_random.run_pipeline()
+    if pipeline_name == "PopularityRecommender":
+        pipelines.pipeline_popularity.run_pipeline()
     elif pipeline_name == "SVD":
         pipelines.pipeline_svd.run_pipeline()
     else:
