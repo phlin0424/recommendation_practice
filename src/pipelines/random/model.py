79b3d0784506352892ea295b1a85d43819270a73
PH <jnes40722@gmail.com>
2024-08-22 20:14:17 +0900

    Fix bugs

--- src/pipelines/random/model.py
+++ src/pipelines/random/model.py
@@ -85,7 +85,9 @@ class RandomRecommender(BaseRecommender):
             # Collect the predicted & true ratings
             predict_ratings.append(predict_rating)
 
-        for user_id in self.unique_user_ids:
+        unique_user_ids_test = sorted(set([row.user_id for row in self.train_data]))
+
+        for user_id in unique_user_ids_test:
             # Collect the movie id rated by each user
             all_movie_id_rating = self._predict_rating(user_id)
 
