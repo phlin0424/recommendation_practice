da43b7294ba194deb2da0a512da2c7241c658dd1
PH <jnes40722@gmail.com>
2024-09-12 20:56:13 +0900

    Fix the query

--- src/sql/integrated_tables_tags.sql
+++ src/sql/integrated_tables_tags.sql
@@ -1,15 +1,5 @@
 -- Select data with user_id < user_num (a integer being specified) 
--- Filter the data with limited user number
-WITH users_target AS (
-    SELECT 
-        user_id
-    FROM 
-        ml_10m.users
-    WHERE
-        user_id <   :user_num
-),
--- Split the movie title into the title and the year
-movies_with_years AS (
+WITH movies_with_years AS (
     SELECT 
         movie_id, 
         substring(title FROM '\((\d{4})\)$') AS movie_year, 
@@ -18,124 +8,35 @@ movies_with_years AS (
     FROM
         ml_10m.movies
 ), 
--- Filter the rating data with the limited user number
-ratings AS (
-    SELECT 
-        users_target.user_id as user_id, 
-        movie_id, 
-        rating, 
-        timestamp
-    FROM
-        users_target
-    INNER JOIN
-        (
-            SELECT 
-                user_id, 
-                movie_id, 
-                rating, 
-                timestamp
-            FROM
-                ml_10m.ratings
-        )  AS temp_ratings
-    ON
-        users_target.user_id = temp_ratings.user_id  --filtering off user_id>user_num
-),  
-tags  AS (
+tags AS (
     SELECT 
         movie_id,
-        STRING_AGG(tag, '|') AS concatenated_tags 
-    FROM ml_10m.tags  
-    GROUP BY movie_id 
+        STRING_AGG(tag, '|') AS concatenated_tags
+    FROM 
+        ml_10m.tags  
+    GROUP BY 
+        movie_id 
 ), 
--- connect the rating data to the movie year information
-all_data0 as (
-SELECT 
-    user_id, 
-    ratings.movie_id, 
-    rating, 
-    movie_title, 
-    movie_year,
-    genres, 
-    timestamp
-FROM
-    ratings
-INNER JOIN
-    movies_with_years 
-ON 
-    ratings.movie_id = movies_with_years.movie_id
-),
--- connect the rating data to the tag information
 all_data AS (
 SELECT 
-    user_id, 
-    all_data0.movie_id, 
-    rating, 
+    movies_with_years.movie_id, 
     movie_title, 
     movie_year,
     genres, 
-    timestamp,
-    COALESCE(concatenated_tags, '') AS tag  -- Replace NULL with an empty string
+    COALESCE(concatenated_tags, '') AS tags  -- Replace NULL with an empty string
 FROM 
-    all_data0
+    movies_with_years
 LEFT JOIN
     tags
 ON 
-    all_data0.movie_id = tags.movie_id
-),
--- split the data into test and training data
-ranked_data AS (
-    SELECT 
-        user_id, 
-        movie_id, 
-        rating, 
-        movie_title, 
-        movie_year, 
-        genres, 
-        tag, 
-        timestamp, 
-        ROW_NUMBER() OVER (PARTITION BY user_id ORDER BY timestamp desc) AS rank
-    FROM 
-        all_data
-), split_data as (
-SELECT
-    user_id,
-    movie_id,
-    rating,
-    movie_title, 
-    movie_year, 
-    genres, 
-    tag,
-    timestamp,
-    CASE
-        WHEN rank <= 5 THEN 'test'
-        ELSE 'train'
-    END AS label
-FROM
-    ranked_data
-ORDER BY 
-    user_id, timestamp
+    movies_with_years.movie_id = tags.movie_id
 )
+
 SELECT
-    user_id,
     movie_id,
-    rating,
-    movie_title, 
-    movie_year, 
+    movie_title,
+    movie_year,
     genres, 
-    tag,
-    timestamp,
-    label
-FROM
-    split_data
-ORDER BY 
-    user_id, timestamp 
-
-
-
--- rating   count
--- integer  bigint
--- 1	1	5536
--- 2	2	11202
--- 3	3	34217
--- 4	4	47597
--- 5	5	29082
+    tags
+FROM 
+    all_data
