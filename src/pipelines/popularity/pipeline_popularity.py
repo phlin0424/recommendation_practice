8d27dbb5bd89364b53b1e670df1378c49775a1fa
PH <jnes40722@gmail.com>
2024-09-19 20:53:24 +0900

    Update popularity recommender pipeline

--- src/pipelines/popularity/pipeline_popularity.py
+++ src/pipelines/popularity/pipeline_popularity.py
@@ -1,50 +1,63 @@
 import asyncio
-import os
 
-import joblib
 import mlflow
-from core.config import DIR_PATH, settings
+from core.config import settings
+
 from datareader.ml_10m_data import PopularityDatas
 from pipelines.popularity.model import PopularityRecommender
 from utils.evaluation_metrics import Metrics
 from utils.pipeline_logging import configure_logging
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
 logger = configure_logging()
 
 
-def preprocess(user_num) -> PopularityDatas:
-    popularity_datas = asyncio.run(
-        PopularityDatas.from_db(user_num=user_num, threshold=1)
+# pipeline environment variables controller:
+class PipelineSettings(BaseSettings):
+    user_num: int = 1000
+    threshold: int = 10
+    model_name: str = "Popularity"
+
+    model_config = SettingsConfigDict(
+        env_file="pipeline_params.env",
+        env_file_encoding="utf-8",
     )
-    return popularity_datas
 
 
-def train_model(popularity_datas: PopularityDatas) -> PopularityRecommender:
+def preprocess(pipeline_settings: PipelineSettings) -> PopularityRecommender:
+    popularity_datas = asyncio.run(
+        PopularityDatas.from_db(user_num=pipeline_settings.user_num, threshold=1)
+    )
+
+    # Initialize the recommender
     popularity_recommender = PopularityRecommender(popularity_datas)
-    popularity_recommender.train()
+
     return popularity_recommender
 
 
+def train_model(recommender: PopularityRecommender) -> PopularityRecommender:
+    recommender.train()
+    return recommender
+
+
 def evaluate_model(
-    popularity_recommender: PopularityRecommender, threshold: int
+    popularity_recommender: PopularityRecommender,
+    pipeline_settings: PipelineSettings,
 ) -> Metrics:
-    popularity_recommender.predict(threshold=threshold)
+    popularity_recommender.predict(threshold=pipeline_settings.threshold)
     metrics = popularity_recommender.evaluate()
     return metrics
 
 
-def run_pipeline():
-    user_num = int(os.getenv("USER_NUM", "1000"))
-    threshold = int(os.getenv("THRESHOLD", "10"))
-
+def run_pipeline(pipeline_settings: PipelineSettings):
     # Set the tracking URI to the local MLflow server
     tracking_uri = settings.tracking_uri
     mlflow.set_tracking_uri(tracking_uri)
 
-    # Setting of the pipeline
-    model_name = "PopularityRecommender_model"
-    model_output_fname = f"{model_name}.pkl"
-    model_filename = DIR_PATH / f"mlflow/artifacts/{model_output_fname}"
+    # # Setting of the pipeline
+    # model_name = "PopularityRecommender_model"
+    # model_output_fname = f"{model_name}.pkl"
+    # model_filename = DIR_PATH / f"mlflow/artifacts/{model_output_fname}"
 
     # Logging the experiment details
     logger.info(f"MLflow tracking uri: {settings.tracking_uri}")
@@ -52,25 +65,32 @@ def run_pipeline():
     logger.info(f"experiment id: {settings.experiment_id}")
     logger.info(f"experiment name: {settings.experiment_name}")
 
-    with mlflow.start_run(experiment_id=settings.experiment_id) as run:
+    with mlflow.start_run(
+        experiment_id=settings.experiment_id,
+        run_name=pipeline_settings.model_name,
+    ) as run:
+        # ++++++++++++++++++++++++++
         # Preprocess
-        input_data = preprocess(user_num)
-        mlflow.log_param("user_num", user_num)
-        mlflow.log_param("threshold", threshold)
-        mlflow.log_param("model_name", model_name)
-        mlflow.log_param("dataset", "ml-10m")
+        # ++++++++++++++++++++++++++
+        algo = preprocess(pipeline_settings)
+        # joblib.dump(algo, preprocessed_model_filename)
+        # logger.info(f"Preprocessed Model saved to {model_filename}")
 
-        # Train the model, saving the trained model locally, registering the artifact
-        algo = train_model(input_data)
-        joblib.dump(algo, model_filename)
-        mlflow.log_artifact(model_filename, artifact_path="models")
-        logger.info(f"Model saved to {model_filename}")
+        # ++++++++++++++++++++++++++
+        # Train
+        # ++++++++++++++++++++++++++
+        algo = train_model(algo)
+        # joblib.dump(algo, model_filename)
+        # mlflow.log_artifact(model_filename, artifact_path="models")
+        logger.info(f"Model saved to {pipeline_settings.model_name}")
 
+        # ++++++++++++++++++++++++++
         # Predict & Evaluate
-        metrics = evaluate_model(algo, threshold)
-        mlflow.log_metric("rmse", metrics.rmse)
-        mlflow.log_metric("recall_at_k", metrics.recall_at_k)
-        mlflow.log_metric("precision_at_k", metrics.precision_at_k)
+        # ++++++++++++++++++++++++++
+        # Predict & Evaluate
+        metrics = evaluate_model(algo, pipeline_settings)
+        # Log the evaluation results
+        mlflow.log_metrics(metrics.model_dump())
         logger.info(metrics)
 
 
@@ -79,4 +99,8 @@ if __name__ == "__main__":
     # recommender = train_model(input_data)
     # metrics = evaluate_model(recommender, 10)
     # logger.info(metrics)
-    run_pipeline()
+    pipeline_settings = PipelineSettings()
+
+    logger.info(pipeline_settings)
+
+    run_pipeline(pipeline_settings)
