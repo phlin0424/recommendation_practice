c791f18c7fc3d5d3348aab636c3598cbf6cb46ac
PH <jnes40722@gmail.com>
2024-09-19 21:10:35 +0900

    Update SVD recommender pipeline

--- src/pipelines/SVD/pipeline_svd.py
+++ src/pipelines/SVD/pipeline_svd.py
@@ -1,27 +1,47 @@
 import asyncio
-import os
 
-import joblib
 import mlflow
-from core.config import DIR_PATH, settings
+from core.config import settings
 from datareader.ml_10m_data import IntegratedDatas
 from pipelines.SVD.model import SVDRecommender
 from utils.evaluation_metrics import Metrics
 from utils.pipeline_logging import configure_logging
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
 logger = configure_logging()
 
 
-def preprocess(user_num) -> IntegratedDatas:
-    integrated_datas = asyncio.run(IntegratedDatas.from_db(user_num=user_num))
-    return integrated_datas
+# pipeline environment variables controller:
+class PipelineSettings(BaseSettings):
+    user_num: int = 1000
+    fill_with_zero: bool = False
+    model_name: str = "Random"
+    factor: int = 5
+
+    model_config = SettingsConfigDict(
+        env_file="pipeline_params.env",
+        env_file_encoding="utf-8",
+    )
+
+
+def preprocess(pipeline_settings: PipelineSettings) -> SVDRecommender:
+    integrated_datas = asyncio.run(
+        IntegratedDatas.from_db(
+            user_num=pipeline_settings.user_num,
+        )
+    )
+    random_recommender = SVDRecommender(integrated_datas)
+    return random_recommender
 
 
 def train_model(
-    integrated_datas: IntegratedDatas, factor, fill_with_zero
+    random_recommender: SVDRecommender,
+    pipeline_settings: PipelineSettings,
 ) -> SVDRecommender:
-    random_recommender = SVDRecommender(integrated_datas)
-    random_recommender.train(factor=factor, fill_with_zero=fill_with_zero)
+    random_recommender.train(
+        factor=pipeline_settings.factor,
+        fill_with_zero=pipeline_settings.fill_with_zero,
+    )
     return random_recommender
 
 
@@ -31,20 +51,15 @@ def evaluate_model(random_recommender: SVDRecommender) -> Metrics:
     return metrics
 
 
-def run_pipeline():
-    # Load the necessary parameters
-    user_num = int(os.getenv("USER_NUM", "1000"))
-    factor = int(os.getenv("FACTOR", "5"))
-    fill_with_zero = bool(int(os.getenv("FILL_WITH_ZERO", "0")))
-
+def run_pipeline(pipeline_settings: PipelineSettings):
     # Set the tracking URI to the local MLflow server
     tracking_uri = settings.tracking_uri
     mlflow.set_tracking_uri(tracking_uri)
 
-    # Setting of the pipeline
-    model_name = "SVDRecommender_model"
-    model_output_fname = f"{model_name}.pkl"
-    model_filename = DIR_PATH / f"mlflow/artifacts/{model_output_fname}"
+    # # Setting of the pipeline
+    # model_name = "SVDRecommender_model"
+    # model_output_fname = f"{model_name}.pkl"
+    # model_filename = DIR_PATH / f"mlflow/artifacts/{model_output_fname}"
 
     # Logging the experiment details
     logger.info(f"MLflow tracking uri: {settings.tracking_uri}")
@@ -52,30 +67,35 @@ def run_pipeline():
     logger.info(f"experiment id: {settings.experiment_id}")
     logger.info(f"experiment name: {settings.experiment_name}")
 
-    with mlflow.start_run(experiment_id=settings.experiment_id, run_name="SVD") as run:
+    with mlflow.start_run(
+        experiment_id=settings.experiment_id, run_name=pipeline_settings.model_name
+    ) as run:
+        # Log all the pipeline parameters
+        mlflow.log_params(pipeline_settings.model_dump())
+
+        # ++++++++++++++++++++++++++
         # Preprocess
-        input_data = preprocess(user_num)
-        mlflow.log_params(
-            {
-                "user_num": user_num,
-                "model_name": model_name,
-                "dataset": "ml-10m",
-                "factor": factor,
-                "fill_with_zero": fill_with_zero,
-            }
-        )
+        # ++++++++++++++++++++++++++
+        algo = preprocess(pipeline_settings)
+        # joblib.dump(algo, preprocessed_model_filename)
+        # logger.info(f"Preprocessed Model saved to {model_filename}")
 
-        # Train the model, saving the trained model locally, registering the artifact
-        algo = train_model(input_data, factor, fill_with_zero)
-        joblib.dump(algo, model_filename)
-        mlflow.log_artifact(model_filename, artifact_path="models")
-        logger.info(f"Model saved to {model_filename}")
+        # ++++++++++++++++++++++++++
+        # Train
+        # ++++++++++++++++++++++++++
+        algo = train_model(algo, pipeline_settings)
+        # joblib.dump(algo, model_filename)
+        # mlflow.log_artifact(model_filename, artifact_path="models")
+        logger.info(f"Model saved to {pipeline_settings.model_name}")
 
+        # ++++++++++++++++++++++++++
+        # Predict & Evaluate
+        # ++++++++++++++++++++++++++
         # Predict & Evaluate
         metrics = evaluate_model(algo)
-        mlflow.log_metric("rmse", metrics.rmse)
-        mlflow.log_metric("recall_at_k", metrics.recall_at_k)
-        mlflow.log_metric("precision_at_k", metrics.precision_at_k)
+
+        # Log the evaluation results
+        mlflow.log_metrics(metrics.model_dump())
         logger.info(metrics)
 
 
@@ -84,4 +104,8 @@ if __name__ == "__main__":
     # recommender = train_model(input_data)
     # metrics = evaluate_model(recommender)
     # logger.info(metrics)
-    run_pipeline()
+    pipeline_settings = PipelineSettings()
+
+    logger.info(pipeline_settings)
+
+    run_pipeline(pipeline_settings)
